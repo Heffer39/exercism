a3dc3eb3f4702eb11cdf49ea0d8cf45cd1847b3d
Ryan Cerankowski <rcerankowski@gmail.com>
2020-08-24 16:30:24 -0400

    Anagram & Luhn WIP

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,1 +1,14 @@
 package anagram
+
+import "sort"
+
+func Detect(subject string, candidates []string) (result []string){
+	sorted := subject
+
+	for s := range candidates {
+		s = sort.Strings(s)
+		if
+	}
+
+	return
+}
