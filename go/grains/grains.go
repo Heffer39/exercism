2cd1bf6046cc10dba5691172239878a32e31ce58
Ryan Cerankowski <rcerankowski@gmail.com>
2020-08-28 16:05:20 -0400

    Updated Grains solution
    Utilized the left shift operator

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -4,7 +4,6 @@ package grains
 
 import (
 	"fmt"
-	"math"
 )
 
 // Square calculates the number of grains in a given square on a chess board
@@ -12,17 +11,24 @@ func Square(position int) (uint64, error) {
 	if position <= 0 || position > 64 {
 		return 0, fmt.Errorf("position %v does not fit within a 64 square chessboard", position)
 	}
-	return uint64(math.Exp2(float64(position - 1))), nil
+	return 1 << (position - 1), nil
 }
 
 // Total is a deterministic method that will compute the sum of all squares on a chessboard with 64 squares
 // The answer is always equal to 18446744073709551615
 // Errors are unhandled because we will never capture the "expectedError"
 func Total() uint64 {
-	var sum uint64 = 0
-	for i := 1; i <= 64; i++ {
-		square, _ := Square(i)
-		sum = sum + square
-	}
-	return sum
+	return 1<<(64) - 1
 }
+
+/*
+func Total() uint64 {
+	total, err := Square(64)
+
+	if err != nil {
+		panic("error blah")
+	}
+
+	return total*2 - 1
+}
+*/
