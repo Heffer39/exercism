3b755027afa96579687995af4bed51fc40b2a9c3
Ryan Cerankowski <rcerankowski@gmail.com>
2020-09-03 12:40:48 -0400

    Anagram submission

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,14 +1,40 @@
+// Package anagram can help determine if words are anagrams of each other
 package anagram
 
-import "sort"
+import (
+	"strings"
+)
 
-func Detect(subject string, candidates []string) (result []string){
-	sorted := subject
+// Detect selects the sublist of anagrams for the given word out of the list of candidates
+func Detect(subject string, candidates []string) (anagrams []string) {
+	for _, candidate := range candidates {
+		if compareTwoStrings(strings.ToLower(subject), strings.ToLower(candidate)) {
+			anagrams = append(anagrams, candidate)
+		}
+	}
+	return
+}
 
-	for s := range candidates {
-		s = sort.Strings(s)
-		if
+// compareTwoStrings checks two individual words to determine if they are anagrams
+func compareTwoStrings(subject, candidate string) bool {
+	if len(subject) != len(candidate) || subject == candidate {
+		return false
+	}
+	subjectRuneMap := map[rune]int{}
+	candidateRuneMap := map[rune]int{}
+
+	for _, c := range subject {
+		subjectRuneMap[c]++
+	}
+	for _, c := range candidate {
+		candidateRuneMap[c]++
 	}
 
-	return
+	for c := range subjectRuneMap {
+		if subjectRuneMap[c] != candidateRuneMap[c] {
+			return false
+		}
+	}
+
+	return true
 }
