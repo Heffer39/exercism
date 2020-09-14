74cbaf23f62b56406e792e038503857f820b71c2
Ryan Cerankowski <rcerankowski@gmail.com>
2020-09-14 13:52:20 -0400

    Added normHoursAndMinutes function

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -11,19 +11,11 @@ type Clock struct {
 	minute int
 }
 
-// New acts as the constructor for the Clock type
-func New(hour, minute int) Clock {
-	c := new(Clock)
-	c.hour, c.minute = norm(hour, minute, 60)
-	_, c.hour = norm(1, c.hour, 24)
-	return *c
-}
-
 // norm returns nhi, nlo such that
 //	hi * base + lo == nhi * base + nlo
 //	0 <= nlo < base
 // Copied private method norm from Package time
-func norm(hi, lo, base int) (nhi, nlo int) {
+func norm(hi, lo, base int) (int, int) {
 	if lo < 0 {
 		n := (-lo-1)/base + 1
 		hi -= n
@@ -37,17 +29,30 @@ func norm(hi, lo, base int) (nhi, nlo int) {
 	return hi, lo
 }
 
+// normHoursAndMinutes calls the norm function and returns hours and minutes
+// in a 24 hour, 60 minute clock format
+func normHoursAndMinutes(hour, minute int) (int, int) {
+	hour, minute = norm(hour, minute, 60)
+	hour = (24 + hour%24) % 24
+	return hour, minute
+}
+
+// New acts as the constructor for the Clock type
+func New(hour, minute int) Clock {
+	c := new(Clock)
+	c.hour, c.minute = normHoursAndMinutes(hour, minute)
+	return *c
+}
+
 // Add takes a clock and adds minutes to it
 func (c Clock) Add(add int) Clock {
-	c.hour, c.minute = norm(c.hour, c.minute+add, 60)
-	_, c.hour = norm(0, c.hour, 24)
+	c.hour, c.minute = normHoursAndMinutes(c.hour, c.minute+add)
 	return c
 }
 
 // Subtract takes a clock and subtracts minutes from it
 func (c Clock) Subtract(subtract int) Clock {
-	c.hour, c.minute = norm(c.hour, c.minute-subtract, 60)
-	_, c.hour = norm(0, c.hour, 24)
+	c.hour, c.minute = normHoursAndMinutes(c.hour, c.minute-subtract)
 	return c
 }
 
