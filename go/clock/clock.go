d61cd603f608befa50367b1ef183d78427613972
Ryan Cerankowski <rcerankowski@gmail.com>
2020-09-17 20:46:30 -0400

    Removed hours from Clock struct
    Refactore the New function to only handle minutes

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -5,45 +5,33 @@ import (
 	"fmt"
 )
 
-// Clock represents a 24 hour clock that captures the hour and minute of a given time
+// Clock represents a 24 hour clock that captures the total minutes of a given time
 type Clock struct {
-	hour   int
-	minute int
+	minutes int
 }
 
-// New acts as the constructor for the Clock type and
-// returns hours and minutes in a 24 hour, 60 minute clock format
+const minutesInOneDay int = 60 * 24
+
+// New acts as the constructor for the Clock struct
 func New(hour, minute int) Clock {
-	c := new(Clock)
-	base := 60
-
-	if minute < 0 {
-		n := (-minute-1)/base + 1
-		hour -= n
-		minute += n * base
+	c := &Clock{(hour*60 + minute) % minutesInOneDay}
+	if c.minutes < 0 {
+		c.minutes += minutesInOneDay
 	}
-	if minute >= base {
-		n := minute / base
-		hour += n
-		minute -= n * base
-	}
-
-	c.hour, c.minute = hour, minute
-	c.hour = (24 + c.hour%24) % 24
 	return *c
 }
 
 // Add takes a clock and adds minutes to it
 func (c Clock) Add(add int) Clock {
-	return New(c.hour, c.minute+add)
+	return New(0, c.minutes+add)
 }
 
 // Subtract takes a clock and subtracts minutes from it
 func (c Clock) Subtract(subtract int) Clock {
-	return New(c.hour, c.minute-subtract)
+	return New(0, c.minutes-subtract)
 }
 
 // String takes a clock and prints out the hours and minutes in "00:00" 24h format
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	return fmt.Sprintf("%02d:%02d", c.minutes/60, c.minutes%60)
 }
