7ac5233b8f59aa8de4f007e1b461e303eaed323a
Ryan Cerankowski <rcerankowski@gmail.com>
2020-10-17 15:31:53 -0400

    Initial solution

---
+++ go/robot-name/robot_name.go
@@ -0,0 +1,57 @@
+package robotname
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+// Robot contains the name for a robot
+type Robot struct {
+	name string
+}
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numberset = "0123456789"
+
+var usedNames = map[string]bool{}
+
+// Name generates a name for the given robot while verifying that
+// the randomly generated name hasn't been duplicated and all names have not been assigned
+func (r *Robot) Name() (string, error) {
+	if len(usedNames) == 26*26*10*10*10 {
+		return "", errors.New("all possible names have been used")
+	}
+
+	if r.name == "" {
+		r.name = generateName()
+		for usedNames[r.name] {
+			r.name = generateName()
+		}
+		usedNames[r.name] = true
+	}
+
+	return r.name, nil
+}
+
+// generateName populates a Robot's name as a 5 character string
+// The first two characters are uppercase letters
+// The last three characters are numbers
+func generateName() string {
+	b := make([]byte, 5)
+	for i := 0; i < 5; i++ {
+		if i < 2 {
+			b[i] = charset[seededRand.Intn(len(charset))]
+		} else {
+			b[i] = numberset[seededRand.Intn(len(numberset))]
+		}
+	}
+	return string(b)
+}
+
+// Reset simply sets the Robot's name to a blank string
+func (r *Robot) Reset() {
+	r.name = ""
+}
