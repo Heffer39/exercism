e06661e3c011b927702849b6a7816c9334af0ae1
Ryan Cerankowski <rcerankowski@gmail.com>
2020-10-21 15:12:08 -0400

    Final Solution

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -2,6 +2,7 @@ package robotname
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -18,16 +19,17 @@ var usedNames = map[string]bool{}
 // Name generates a name for the given robot while verifying that
 // the randomly generated name hasn't been duplicated and all names have not been assigned
 func (r *Robot) Name() (string, error) {
-	if r.name == "" {
-		if len(usedNames) == 26*26*10*10*10 {
-			return "", errors.New("all possible names have been used")
-		}
-		r.name = generateName()
-		for usedNames[r.name] {
-			r.name = generateName()
-		}
-		usedNames[r.name] = true
+	if r.name != "" {
+		return r.name, nil
 	}
+	if len(usedNames) == 26*26*10*10*10 {
+		return "", errors.New("all possible names have been used")
+	}
+	r.name = generateName()
+	for usedNames[r.name] {
+		r.name = generateName()
+	}
+	usedNames[r.name] = true
 
 	return r.name, nil
 }
@@ -36,15 +38,10 @@ func (r *Robot) Name() (string, error) {
 // The first two characters are uppercase letters
 // The last three characters are numbers
 func generateName() string {
-	b := make([]byte, 5)
-	for i := 0; i < 5; i++ {
-		if i < 2 {
-			b[i] = byte(seededRand.Intn(26) + 'A')
-		} else {
-			b[i] = byte(seededRand.Intn(10) + '0')
-		}
-	}
-	return string(b)
+	r1 := seededRand.Intn(26) + 'A'
+	r2 := seededRand.Intn(26) + 'A'
+	num := seededRand.Intn(1000)
+	return fmt.Sprintf("%c%c%03d", r1, r2, num)
 }
 
 // Reset simply sets the Robot's name to a blank string
