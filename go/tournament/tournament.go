f8a30b0a6400e14a34afa5a91c6f215e9a16958c
Ryan Cerankowski <rcerankowski@gmail.com>
2020-10-22 08:23:09 -0400

    Initial Submission
    Introduced new sortedTeamList function
    Added comments
    General cleanup

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -1,3 +1,4 @@
+//Package tournament tallies the results of a small football competition
 package tournament
 
 import (
@@ -5,11 +6,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
 	"sort"
 	"strings"
 )
 
+// team captures the name and stats of a team for the competition
 type team struct {
 	name          string
 	matchesPlayed int
@@ -21,38 +22,35 @@ type team struct {
 
 var teamsInTournament map[string]*team
 
+// Tally reads in an input file, calculates the match results, and writes back
+// the results to the io.Writer
 func Tally(r io.Reader, w io.Writer) error {
 	teamsInTournament = make(map[string]*team)
-
 	scanner := bufio.NewScanner(r)
 
 	for scanner.Scan() {
-		//fmt.Println(scanner.Text())
 		s := strings.SplitN(scanner.Text(), ";", 3)
 		if len(s) != 3 && len(s) > 1 {
-			//fmt.Println(s)
-			//fmt.Println(len(s))
 			return errors.New("incorrect size")
 		}
 		if len(s) == 3 {
-			err := calculateMatchResults(s)
-			if err != nil {
+			if err := calculateMatchResults(s); err != nil {
 				return err
 			}
 		}
-
-		//fmt.Println(scanner.Text()) // Println will add back the final '\n'
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		return err
 	}
 
-	w.Write([]byte(printResults()))
+	teamsList := sortedTeamList()
+	if _, err := w.Write([]byte(printResults(teamsList))); err != nil {
+		return err
+	}
 	return nil
-	//return errors.New("")
 }
 
+// calculateMatchResults updates the stats for each team, based on the provided match information
 func calculateMatchResults(s []string) error {
 	if _, ok := teamsInTournament[s[0]]; !ok {
 		teamsInTournament[s[0]] = &team{name: s[0]}
@@ -67,26 +65,26 @@ func calculateMatchResults(s []string) error {
 
 	switch s[2] {
 	case "draw":
-		teamsInTournament[s[0]].matchesPlayed += 1
-		teamsInTournament[s[0]].draws += 1
-		teamsInTournament[s[0]].points += 1
+		teamsInTournament[s[0]].matchesPlayed++
+		teamsInTournament[s[0]].draws++
+		teamsInTournament[s[0]].points++
 
-		teamsInTournament[s[1]].matchesPlayed += 1
-		teamsInTournament[s[1]].draws += 1
-		teamsInTournament[s[1]].points += 1
+		teamsInTournament[s[1]].matchesPlayed++
+		teamsInTournament[s[1]].draws++
+		teamsInTournament[s[1]].points++
 	case "win":
-		teamsInTournament[s[0]].matchesPlayed += 1
-		teamsInTournament[s[0]].wins += 1
+		teamsInTournament[s[0]].matchesPlayed++
+		teamsInTournament[s[0]].wins++
 		teamsInTournament[s[0]].points += 3
 
-		teamsInTournament[s[1]].matchesPlayed += 1
-		teamsInTournament[s[1]].losses += 1
+		teamsInTournament[s[1]].matchesPlayed++
+		teamsInTournament[s[1]].losses++
 	case "loss":
-		teamsInTournament[s[0]].matchesPlayed += 1
-		teamsInTournament[s[0]].losses += 1
+		teamsInTournament[s[0]].matchesPlayed++
+		teamsInTournament[s[0]].losses++
 
-		teamsInTournament[s[1]].matchesPlayed += 1
-		teamsInTournament[s[1]].wins += 1
+		teamsInTournament[s[1]].matchesPlayed++
+		teamsInTournament[s[1]].wins++
 		teamsInTournament[s[1]].points += 3
 	default:
 		return errors.New("incorrect input")
@@ -94,7 +92,10 @@ func calculateMatchResults(s []string) error {
 	return nil
 }
 
-func printResults() string {
+// sortedTeamList returns a sorted list of all the teams in the tournament
+// Teams are sorted by points
+// If teams are tied on points, they are instead sorted alphabetically
+func sortedTeamList() []team {
 	var teams []team
 	for _, value := range teamsInTournament {
 		teams = append(teams, *value)
@@ -107,6 +108,11 @@ func printResults() string {
 		return teams[i].points > teams[j].points
 	})
 
+	return teams
+}
+
+// printResults returns a formatted string, displaying the tabular data of each team and their stats
+func printResults(teams []team) string {
 	var result strings.Builder
 
 	result.WriteString(
@@ -114,7 +120,7 @@ func printResults() string {
 	)
 
 	for _, team := range teams {
-		s := fmt.Sprintf("%-31s|  %d |  %d |  %d |  %d |  %d\n",
+		s := fmt.Sprintf("%-31s| %2d | %2d | %2d | %2d | %2d\n",
 			team.name,
 			team.matchesPlayed,
 			team.wins,
@@ -123,7 +129,6 @@ func printResults() string {
 			team.points)
 		result.WriteString(s)
 	}
-	//result.WriteString("\n")
 
 	return result.String()
 }
