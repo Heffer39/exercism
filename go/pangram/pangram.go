c828eb7e052468fbce5d5cd40ada0cd76a075c79
Ryan Cerankowski <rcerankowski@gmail.com>
2020-10-28 16:59:27 -0400

    Pangram final submission
    Publishing clean ContainsRune solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -7,17 +7,6 @@ import (
 
 // IsPangram returns true if the given sentence is a pangram
 func IsPangram(sentence string) bool {
-	/* Map approach is O(n) for speed, but is much slower for the benchmark cases
-	var pangramCheck = make(map[rune]bool, 26)
-	s := strings.ToLower(sentence)
-	for _, v := range s {
-		if unicode.IsLetter(v) {
-			pangramCheck[v] = true
-		}
-	}
-	return len(pangramCheck) == 26
-	*/
-	// ContainsRune approach is O(n^2) for speed, but is much faster for the benchmark cases
 	s := strings.ToLower(sentence)
 	for r := 'a'; r <= 'z'; r++ {
 		if !strings.ContainsRune(s, r) {
