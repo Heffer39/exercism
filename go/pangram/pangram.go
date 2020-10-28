501d1d9610e0e28a649cf9d119bc394cea860e67
Ryan Cerankowski <rcerankowski@gmail.com>
2020-10-28 16:58:32 -0400

    Pangram dual solutions
    Provided both O(n) and O(n^2) solutions, even though the O(n^2) is better for this particular problem

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -2,50 +2,25 @@
 package pangram
 
 import (
-	"unicode"
+	"strings"
 )
 
 // IsPangram returns true if the given sentence is a pangram
 func IsPangram(sentence string) bool {
-	var pangramCheck = map[rune]int{
-		'a': 0,
-		'b': 0,
-		'c': 0,
-		'd': 0,
-		'e': 0,
-		'f': 0,
-		'g': 0,
-		'h': 0,
-		'i': 0,
-		'j': 0,
-		'k': 0,
-		'l': 0,
-		'm': 0,
-		'n': 0,
-		'o': 0,
-		'p': 0,
-		'q': 0,
-		'r': 0,
-		's': 0,
-		't': 0,
-		'u': 0,
-		'v': 0,
-		'w': 0,
-		'x': 0,
-		'y': 0,
-		'z': 0,
-	}
-
-	for _, v := range sentence {
-		if _, ok := pangramCheck[v]; !ok {
-			if string(v) == " " || string(v) == "_" || unicode.IsNumber(v) {
-				continue
-			}
+	/* Map approach is O(n) for speed, but is much slower for the benchmark cases
+	var pangramCheck = make(map[rune]bool, 26)
+	s := strings.ToLower(sentence)
+	for _, v := range s {
+		if unicode.IsLetter(v) {
+			pangramCheck[v] = true
 		}
-		pangramCheck[unicode.ToLower(v)]++
 	}
-	for _, v := range pangramCheck {
-		if v == 0 {
+	return len(pangramCheck) == 26
+	*/
+	// ContainsRune approach is O(n^2) for speed, but is much faster for the benchmark cases
+	s := strings.ToLower(sentence)
+	for r := 'a'; r <= 'z'; r++ {
+		if !strings.ContainsRune(s, r) {
 			return false
 		}
 	}
