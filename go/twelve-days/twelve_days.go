ec0d95d8b8dd59979bace3ee53ffc64c6a60bc0a
Ryan Cerankowski <rcerankowski@gmail.com>
2020-10-30 08:10:30 -0400

    Pleasantly trivial solution

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -3,61 +3,31 @@ package twelve
 
 import "strings"
 
-var christmasGifts = map[int]string{
-	1:  "a Partridge in a Pear Tree",
-	2:  "two Turtle Doves",
-	3:  "three French Hens",
-	4:  "four Calling Birds",
-	5:  "five Gold Rings",
-	6:  "six Geese-a-Laying",
-	7:  "seven Swans-a-Swimming",
-	8:  "eight Maids-a-Milking",
-	9:  "nine Ladies Dancing",
-	10: "ten Lords-a-Leaping",
-	11: "eleven Pipers Piping",
-	12: "twelve Drummers Drumming",
-}
-
-var dayNumbers = map[int]string{
-	1:  "first",
-	2:  "second",
-	3:  "third",
-	4:  "fourth",
-	5:  "fifth",
-	6:  "sixth",
-	7:  "seventh",
-	8:  "eighth",
-	9:  "ninth",
-	10: "tenth",
-	11: "eleventh",
-	12: "twelfth",
+var verses = []string{
+	"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+	"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
 }
 
 // Song returns a string of all twelve verses, for each day in the twelve days of christmas song
 func Song() string {
-	var verses []string
+	var v []string
 	for i := 1; i <= 12; i++ {
-		verses = append(verses, Verse(i))
+		v = append(v, Verse(i))
 	}
-	return strings.Join(verses, "\n")
+	return strings.Join(v, "\n")
 }
 
 // Verse returns a string of the verse for a specific day in the twelve days of christmas song
 func Verse(n int) string {
-	var verse strings.Builder
-	beginning := "On the " + dayNumbers[n] + " day of Christmas my true love gave to me: "
-	verse.WriteString(beginning)
-	for i := n; i > 0; i-- {
-		if i == 1 {
-			finalString := christmasGifts[i] + "."
-			if n > 1 {
-				finalString = "and " + finalString
-			}
-			verse.WriteString(finalString)
-			break
-		}
-		day := christmasGifts[i] + ", "
-		verse.WriteString(day)
-	}
-	return verse.String()
+	return verses[n-1]
 }
