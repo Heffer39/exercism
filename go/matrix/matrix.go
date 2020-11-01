cc0ec3a8f5e2de2b648ee89dbb7356b13fa0dbd1
Ryan Cerankowski <rcerankowski@gmail.com>
2020-11-01 09:48:26 -0500

    Matrix initial submission

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -1,3 +1,4 @@
+// Package matrix implements matrix operations
 package matrix
 
 import (
@@ -6,49 +7,33 @@ import (
 	"strings"
 )
 
+// Matrix represents a two dimension slice of ints
 type Matrix [][]int
 
+// New returns a new two dimensional int matrix from a given string
 func New(s string) (Matrix, error) {
-	//fmt.Printf("Problem:\n%s\n", s)
 	rows := strings.Split(s, "\n")
-	//_, _ = fmt.Printf("rows: %s\n", rows)
 	matrix := make([][]int, len(rows))
+	lenCols := len(strings.Split(strings.Trim(rows[0], " "), " "))
 	for i, row := range rows {
-		row = strings.Trim(row, " ")
-		nums := strings.Split(row, " ")
-		//_, _ = fmt.Printf("row: %s\n", row)
-		//_, _ = fmt.Println(nums)
+		nums := strings.Split(strings.Trim(row, " "), " ")
+		if len(nums) != lenCols {
+			return nil, fmt.Errorf("invalid matrix dimensions")
+		}
 		matrix[i] = make([]int, len(nums))
 		for j, v := range nums {
-			if v == " " {
-				continue
-			}
 			num, err := strconv.Atoi(v)
 			if err != nil {
 				return nil, fmt.Errorf("invalid input %s", v)
 			}
 			matrix[i][j] = num
-			/*if !unicode.IsNumber(v) && !unicode.IsSpace(v) {
-				return nil, fmt.Errorf("invalid input")
-			}
-			matrix[i][j] = int(v - '0')*/
 		}
-	}
-	//fmt.Printf("Matrix:\n%v\n", matrix)
-
-	var rowSize int
-	for _, row := range matrix {
-		if rowSize == 0 {
-			rowSize = len(row)
-		}
-		if rowSize != len(row) {
-			return nil, fmt.Errorf("invalid matrix dimensions")
-		}
-		//fmt.Printf("row size: %v\n", len(row))
 	}
 	return matrix, nil
 }
 
+// Cols returns a list of columns from the original matrix, reading each column from top to bottom
+// while moving from left to right across the rows
 func (m *Matrix) Cols() [][]int {
 	matrix := make([][]int, len((*m)[0]))
 	for i, row := range *m {
@@ -56,19 +41,17 @@ func (m *Matrix) Cols() [][]int {
 			if i == 0 {
 				matrix[j] = make([]int, len(*m))
 			}
-			//fmt.Printf("val %v\n", v)
 			matrix[j][i] = v
 		}
 	}
-	//fmt.Printf("Col matrix:\n%v\n", matrix)
 	return matrix
 }
 
+// Rows returns a list of rows in the original matrix, reading each row from left to right
+// while moving top to bottom across the rows
 func (m *Matrix) Rows() [][]int {
 	matrix := make([][]int, len(*m))
-	//fmt.Printf("row: matrix length %v\n", len(matrix))
 	for i, row := range *m {
-		//fmt.Printf("row: matrix row length %v\n", len(row))
 		matrix[i] = make([]int, len(row))
 		for j, v := range row {
 			matrix[i][j] = v
@@ -77,14 +60,12 @@ func (m *Matrix) Rows() [][]int {
 	return matrix
 }
 
+// Set places the given value in the specified [row][column] coordinates
 func (m *Matrix) Set(row int, column int, value int) bool {
-	//fmt.Printf("Set matrix:\n%v\n", *m)
 	if row < 0 || column < 0 || row > len(*m)-1 || column > len((*m)[row])-1 {
-		fmt.Println("bad input")
+		fmt.Println("invalid input")
 		return false
 	}
 	(*m)[row][column] = value
-	//fmt.Printf("Set matrix update:\n%v\n", *m)
-	//fmt.Printf("row: %v, col: %v, val: %v\n", row, column, value)
 	return true
 }
